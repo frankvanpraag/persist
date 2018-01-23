17815b8b9cc1e63ec4f90d16d03b2787c2900ba8
-FvP- <drupal@vanpraag.com>
2018-01-24 08:42:05 +1100

    port 80

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var   env          = process.env;
 var app = express();
 
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 80);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
